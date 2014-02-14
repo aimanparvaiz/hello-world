e83490906a1e473dad2dd6e1768f7b751a696ec6
aimanparvaiz <aimanparavaiz@gmail.com>
2014-02-13 18:37:26 -0800

    Adding comments about function task

--- manager.py
+++ manager.py
@@ -10,12 +10,25 @@ from fabric.operations import prompt
 from yaml import load, safe_dump
 
 @task
-def add_ip(zone, server_type, port, ip, user):
+def add_ip(zone, server_type, port, ip, requester_name):
+	"""
+	Opens port for a particular IP. Saves the name of the requester.
+
+	:param zone: Environments; prod, stage, dev.
+	:param server_type: Server type under consideration.
+	:param requester_name: Whose IP is being added.
+
+	"""
+
+
+@task
+def remove_ip(zone, server_type, port, ip, requester_name):
 	pass
 
 @task
-def remove_ip(zone, server_type, port, ip, user):
+def build_group_db(zone):
 	pass
+
 
 
 

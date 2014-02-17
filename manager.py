5021873d6418b9a1c4f0caee4e78718c286d961f
aimanparvaiz <aimanparavaiz@gmail.com>
2014-02-17 13:33:02 -0800

    Adding error handling and completing add_ip

--- manager.py
+++ manager.py
@@ -33,16 +33,36 @@ def get_db_conn():
 def db_initializer():
 	conn = get_db_conn()
 	c = conn.cursor()
+	# Do exception handling may be
 	c.execute('''CREATE TABLE sec_grp_info(region text, security_group text, port real, ip text, requester_name text)''')
+
+def db_write():
+	db_conn = get_db_conn()
+	c = db_conn.cursor()
+
+	try:
+		os.path.exists(db_path)
+	except IOError:
+		print('DB in not initialized, initializing it')
+		# Call DB initializer()
+		db_initializer()
+
+	if not c.execute("INSERT INTO sec_grp_info VALUES(region, security_group, port, ip, requester_name)"):
+		ex = AppError('DB insert failed')
+		raise ex
 
 @task
 def open_port(sec_grp, ip_protocol, port, cidr_ip):
 	# If API call is successful then in returns True
 	if not sec_grp.authorize(ip_protocol='tcp', from_port=port, to_port=port, cidr_ip=cidr_ip):
-		ex = AppError( "AWS API failed" )
+		ex = AppError( "Adding security group rule failed" )
+		raise ex
+@task
+def remove_rule(sec_grp, ip_protocol, port, cidr_ip):
+	if not sec_grp.revoke(ip_protocol='tcp', from_port=port, to_port=port, cidr_ip=cidr_ip)
+		ex = AppError("Removing security group rule failed")
 		raise ex
 
-# TODO Make this threaded for multi simultaneous executions
 @task
 def add_ip(region, group, port, ip, requester_name):
 	"""
@@ -54,9 +74,7 @@ def add_ip(region, group, port, ip, requester_name):
 
 	"""
 	cidr_ip = ip+'/32'
-	#Zone is the key to get vpc-id from config. Zone will also give
-	#all the sec grps. From these sec grps get the one containing the word
-	#server_type and then perform the action on this grp
+	sec_grp_string = 'SecurityGroup:'+group
 
 	# TODO: IP range
 	conn = get_ec2(region)
@@ -64,45 +82,30 @@ def add_ip(region, group, port, ip, requester_name):
 	sec_grps = conn.get_all_security_groups()
 
 	for count in sec_grps:
-		#import ipdb; ipdb.set_trace()
-		if str(count) == 'SecurityGroup:'+group:
+		if str(count) == sec_grp_string:
 			sec_grp = count
 	# This should be checked from the yaml
 	if len(str(sec_grp)) == 0:
 		print('sec grp not found')
 		sys.exit(0)
 
-	# Write a function which opens the security group and let it throw a exception.
-	# Catch this exception and in the catch part undo the action.
-
 	try:
 		open_port(sec_grp, ip_protocol, port, cidr_ip)
 	except AppError, ex
 		print ex
-
+		sys.exit(0)
+		# Exit gracefully
 
 	try:
 		db_write()
-	excpet AppError, ex
+	except AppError, ex
 		print ex
 		# Undo the actions
-
-	if sec_grp.authorize(ip_protocol='tcp', from_port=port, to_port=port, cidr_ip=cidr_ip):
 		try:
-			os.path.exists('/mnt/sec_grp.db')
-		except IOError:
-			print('DB in not initialized, initializing it')
-		# Call DB initializer()
-
-		db_conn = get_db_conn()
-		c = db_conn.cursor()
-		try:
-			c.execute("INSERT INTO sec_grp_info VALUES(region, security_group, port, ip, requester_name)")
-		except IOError:
-			print('Operation failed')
-			sys.exit(0)
-	else:
-		print ('Security Grp call failed')
+			remove_rule(sec_grp, ip_protocol, port, cidr_ip)
+		except AppError, ex
+			print ex
+			print ('Remove rule manually')
 
 
 

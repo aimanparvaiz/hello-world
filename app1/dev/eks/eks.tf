d1513e9c0e2d5ef6dd96b9d230c3763378509d72
Aiman Parvaiz <aimanparvaiz@gmail.com>
2018-12-01 00:57:23 -0500

    Adding EKS TF

---
+++ app1/dev/eks/eks.tf
@@ -0,0 +1,50 @@
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config {
+    bucket = "apz-tf-state"
+    key = "us-west-2/dev/vpc/terraform.tfstate"
+
+    region = "us-west-2"
+  }
+}
+locals {
+  worker_groups = [
+    {
+      instance_type = "t2.small"
+      asg_max = 4
+      autoscaling_enabled = true
+    }
+  ]
+}
+
+module "eks" {
+  source = "git::ssh://git@github.com/aimanparvaiz/tf-modules.git//modules/eks"
+  cluster_name = "app1-dev-eks"
+  vpc_id = "${data.terraform_remote_state.vpc.vpc_id}"
+  private_subnets = ["${data.terraform_remote_state.vpc.private_subnets}"]
+  worker_group_count = 1
+  worker_groups = "${local.worker_groups}"
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+
+}
+
+output "cluster_certificate_authority_data" {
+  value = "${module.eks.cluster_certificate_authority_data}"
+}
+
+output "cluster_endpoint" {
+  value = "${module.eks.cluster_endpoint}"
+}
+
+output "cluster_id" {
+  value = "${module.eks.cluster_id}"
+}
+output "kubeconfig" {
+  value = "${module.eks.kubeconfig}"
+}
+output "worker_security_group_id" {
+  value = "${module.eks.worker_security_group_id}"
+}
---
+++ app1/dev/eks/terraform.tfvars
@@ -0,0 +1,25 @@
+terragrunt = {
+
+  include {
+    path = "${find_in_parent_folders()}"
+  }
+  terraform {
+    source = "."
+
+    extra_arguments "conditional_vars" {
+      commands = [
+        "apply",
+        "plan",
+        "import",
+        "push",
+        "refresh",
+        "destroy"
+      ]
+
+      optional_var_files = [
+        "${get_tfvars_dir()}/../global.tfvars",
+        "${get_tfvars_dir()}/custom.tfvars",
+      ]
+    }
+  }
+}

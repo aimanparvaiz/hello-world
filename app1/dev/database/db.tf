38448b3d25d04b80e3bdfdee0ad1ee03623abb35
Aiman Parvaiz <aimanparvaiz@gmail.com>
2018-12-01 00:59:45 -0500

    Adding DB TF

---
+++ app1/dev/database/db.tf
@@ -0,0 +1,27 @@
+data "terraform_remote_state" "eks" {
+  backend = "s3"
+  config {
+    bucket = "apz-tf-state"
+    key = "us-west-2/dev/eks/terraform.tfstate"
+
+    region = "us-west-2"
+  }
+}
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config {
+    bucket = "apz-tf-state"
+    key = "us-west-2/dev/vpc/terraform.tfstate"
+
+    region = "us-west-2"
+  }
+}
+module "db" {
+  source = "git::ssh://git@github.com/aimanparvaiz/tf-modules.git//modules/db"
+  identifier = "app1-dev-db"
+  name     = "demodb"
+  vpc_sg_id = ["${data.terraform_remote_state.eks.worker_security_group_id}"]
+  env       = "dev"
+  vpc_subnets = "${data.terraform_remote_state.vpc.private_subnets}"
+
+}
